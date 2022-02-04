39503448de056e9237cd48fbf4fd3e0a355bf8a0
66ed3gs <66ed3gs@gmail.com>
2022-02-04 21:52:57 +0900

    Add: new next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  distDir: 'build',
   reactStrictMode: true,
-}
+  poweredByHeader: false,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

10920dc696517cb1a901d66e97a8e820818acc78
Oishi Akito <66ed3gs@gmail.com>
2022-02-07 22:51:13 +0900

    Create Language switch button in DrawerMenu (#26)
    Add: lang change button

--- src/components/Header/DrawerMenu.tsx
+++ src/components/Header/DrawerMenu.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useRouter } from 'next/router';
 import {
   useDisclosure,
   useColorModeValue,
@@ -11,17 +12,27 @@ import {
   DrawerCloseButton,
   DrawerHeader,
   DrawerBody,
+  DrawerFooter,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuOptionGroup,
+  MenuItemOption,
   Button,
   Icon,
 } from '@chakra-ui/react';
 import { FiMenu, FiUploadCloud } from 'react-icons/fi';
+import { MdLanguage } from 'react-icons/md';
 import { useAuth } from 'hooks/auth';
 import Logo from 'components/Header/Logo';
 import Login from 'components/Buttons/Login';
+import { useLocale } from 'hooks/locales';
 
 const DrawerMenu: React.FC = () => {
   const { status } = useAuth();
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const { locale } = useLocale();
+  const router = useRouter();
 
   return (
     <>
@@ -62,6 +73,27 @@ const DrawerMenu: React.FC = () => {
                 <Stack spacing={[2, 4]} fontSize={['1em', '1.05em', '1.1em', '1.2em']}></Stack>
               </Box>
             </DrawerBody>
+            <DrawerFooter>
+              <Box pb={4} w="full" fontSize={['0.65em', '0.85em', '0.9em']} fontWeight="bold">
+                <Menu>
+                  <MenuButton as={Button} leftIcon={<MdLanguage />}>
+                    Change Language
+                  </MenuButton>
+                  <MenuList>
+                    <MenuOptionGroup
+                      defaultValue={locale}
+                      title="Language"
+                      type="radio"
+                      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                      onChange={(value: any) => router.push('/', '/', { locale: value })}
+                    >
+                      <MenuItemOption value="ja">日本語</MenuItemOption>
+                      <MenuItemOption value="en">English</MenuItemOption>
+                    </MenuOptionGroup>
+                  </MenuList>
+                </Menu>
+              </Box>
+            </DrawerFooter>
           </DrawerContent>
         </DrawerOverlay>
       </Drawer>

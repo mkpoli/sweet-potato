9cecf3acb0e0cdd7b844a89c0d94f914116b9e95
gak123 <gak123@yeah.net>
2022-02-18 20:07:39 +0800

    Add chinese translate (#110)
    Add: chinese locale

---
+++ src/locales/ch.ts
@@ -0,0 +1,66 @@
+export default {
+  HEADER: {
+    MYPAGE: '个人主页',
+    UPLOAD: '上传',
+  },
+  TOP_PAGE: {
+    PAGE_TITLE: '最新谱面',
+    LOAD_MORE: '加载更多...',
+    NEW_SCORE_LIST: '最新上传谱面',
+    NO_MORE_SCORE: '没有更多谱面了喵',
+  },
+  UPLOAD_PAGE: {
+    PAGE_TITLE: '上传新谱面',
+    SELECT_FILE: '选择文件',
+    ARTWORK: '歌曲封面',
+    MUSIC_FILE: '音源文件',
+    SCORE_FILE: '谱面文件',
+    UPLOAD: '上传',
+  },
+  UPLOAD_ERROR: {
+    NOT_SUS: '能被支持作为谱面文件的只有后缀名为“.sus”的文件喵。',
+    NEED_MORE_FILE: '你还没有上传需要的文件喵！',
+    OTHER: '呀呀呀！上传似乎失败了。建议过一会再试试呢。',
+  },
+  FORMS: {
+    TITLE: '标题',
+    ARTISTS: '作曲者',
+    AUTHOR: '谱师',
+    GENRE: '分类',
+    RATING: '谱面评分',
+  },
+  GENRE: {
+    GENERAL: '大众',
+    JPOP: '日本流行乐',
+    ANIME: '动漫',
+    VOCALOID: 'Vocaloid',
+  },
+  UPLOAD_GUIDE: {
+    UPLOAD_GUIDELINE: '谱面上传指南',
+    CREATE_CHARTS: '创建新谱面',
+  },
+  FILE_LIST: {
+    CHANGE: '更换文件',
+  },
+  EDIT_PAGE: {
+    PAGE_TITLE: '编辑谱面',
+    PUBLISH: '是否公开？',
+  },
+  DASHBOARD: {
+    SETTINGS: {
+      MENU_TITLE: '设置',
+      TEST: {
+        PAGE_TITLE: '测试服务器URL',
+        TEST_URL: '你的测试服务器URL',
+        COPY: '拷贝',
+      },
+    },
+    CONTENTS: {
+      MENU_TITLE: '内容',
+      LEVELS: {
+        PAGE_TITLE: '已上传谱面',
+        EDIT: '编辑',
+      },
+    },
+  },
+};

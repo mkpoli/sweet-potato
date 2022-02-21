11939330b39616c85111b125529a18645376c10d
CreatorMindGD <100112363+CreatorMindGD@users.noreply.github.com>
2022-02-21 22:00:24 +0900

    Support korean (#132)
    * Create kr.ts
    
    * Update kr.ts
    
    * Update kr.ts
    
    * Update kr.ts

---
+++ src/locales/kr.ts
@@ -0,0 +1,88 @@
+export default {
+  HEADER: {
+    MYPAGE: '마이페이지',
+    UPLOAD: '업로드',
+  },
+  TOP_PAGE: {
+    PAGE_TITLE: '최신 채보',
+    LOAD_MORE: '더 불러오기',
+    NEW_SCORE_LIST: '최신 채보',
+    NO_MORE_SCORE: '모든 채보를 불러왔습니다.',
+  },
+  UPLOAD_PAGE: {
+    PAGE_TITLE: '채보 업로드.',
+    SELECT_FILE: '파일을 선택',
+    ARTWORK: ' 자켓 파일',
+    MUSIC_FILE: '음원 파일',
+    SCORE_FILE: '채보 파일',
+    UPLOAD: '업로드',
+  },
+  UPLOAD_ERROR: {
+    NOT_SUS: '채보파일은 .sus 확장자로 업로드해주셔야 합니다.',
+    NEED_MORE_FILE: '필수로 업로드할 파일들을 전부 업로드하지 못하셨습니다.',
+    OTHER: '업로드 실패. 잠시 후 다시 시도해주세요.',
+  },
+  FORMS: {
+    TITLE: '제목',
+    ARTISTS: '아티스트',
+    AUTHOR: '채보제작자',
+    GENRE: '장르',
+    RATING: '채보의 난이도',
+  },
+  GENRE: { GENERAL: '일반', JPOP: 'J-POP', ANIME: '애니메이션', VOCALOID: '보컬로이드' },
+  UPLOAD_GUIDE: {
+    UPLOAD_GUIDELINE: '업로드 규칙',
+    CREATE_CHARTS: '채보를 만드는 방법',
+  },
+  FILE_LIST: {
+    CHANGE: '변경',
+  },
+  EDIT_PAGE: {
+    PAGE_TITLE: '채보 편집',
+    PUBLISH: '채보를 공개',
+  },
+  DASHBOARD: {
+    SETTINGS: {
+      MENU_TITLE: '설정',
+      TEST: {
+        PAGE_TITLE: '테스트용 서버',
+        TEST_URL: '테스트용 서버 URL',
+        COPY: '복사',
+      },
+    },
+    CONTENTS: {
+      MENU_TITLE: '컨텐츠',
+      LEVELS: {
+        PAGE_TITLE: '업로드한 채보',
+        EDIT: '편집',
+      },
+    },
+  },
+  DOCUMENTS: {
+    GUIDE: {
+      TITLE: '가이드라인',
+      PLAYER: {
+        TITLE: '사용자용',
+        DESCRIPTION: '여기에 사용자가 지켜야 할 가이드라인을 기재하였습니다.',
+        MEDIA: {
+          TITLE: '미디어',
+          TEXT: [
+            '유튜브같은 동영상 업로드 사이트에서 채보를 플레이하는 영상을 업로드하고자 할 때, Sweet Potato에서 공개된 해당 채보의 URL을 같이 기재해주셔야 합니다.',
+            '영상을 업로드할 때 해당 채보, 혹은 그 외적인 요소에 음악, 음성, 이미지 등 여러가지 2차 창작 작품이 포함되어 있습니다. 그러므로 그 작품의 정보를 정확히 표기해주셔야 합니다.',
+            'Sweet Potato은 업로더에 대해 해당 채보에 관련된 2차 창작물의 권리자/소유자가 2차 창작물을 사용하기 위한 별도의 가이드라인을 잘 준수하고 있는지 확인해야할 의무가 있습니다. 또한 2차 창작물을 사용하기 위한 가이드라인이 명시되어 있는 경우, 채보 상세 페이지에 URL을 기재하고 있습니다.',
+            '사용자쪽에서 이를 따르지 않는 경우, 작품의 권리자/소유자로부터 가이드라인을 따르도록 클레임을 걸 가능성이 있습니다.',
+            '덧붙여 2차 창작물을 사용하기 위한 2차 창작물의 권리자/소유자의 가이드라인에 동영상 업로드 사이트를 이용할 때의 지시사항이 표기되어 있는 경우, 사용자의 책임으로 지시사항에 따르는 것으로 합니다.',
+          ],
+        },
+      },
+    },
+    TAP: {
+      TITLE: '약관 및 정책',
+      TOS: '이용약관(일본어)',
+      POLICY: {
+        PRIVACY: '개인정보취급방침(일본어)',
+        DIRECTOR: '서비스 운영자',
+      },
+    },
+  },
+};
